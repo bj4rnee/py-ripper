56d792ab579622e2dcdc01d454280948049dabc1
bj4rnee <bjarne.klar@web.de>
2022-05-30 21:06:17 +0200

    init 2

--- ripper.py
+++ ripper.py
@@ -1,1 +1,8 @@
 from pytube import YouTube
+
+url = ""
+yt = YouTube(url)
+
+yt_stream = yt.streams.get_highest_resolution() # TODO audio only
+print("[Debug] Downloading " + url + " ...")
+yt_stream.download() # TODO args
